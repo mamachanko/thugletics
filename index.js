3f378055e66f1224592d6ce79ec3fc3e52e30f39
mamachanko <sugardubz@gmail.com>
2016-03-29 15:31:33 +0000

    implement most minimal node app serving exercises only

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-yaml = require('js-yaml');
-fs   = require('fs');
+'use strict';
 
-// Get document, or throw exception on error
-try {
-  var doc = yaml.safeLoad(fs.readFileSync('./exercises.yaml', 'utf8'));
-  console.log(doc);
-} catch (e) {
-  console.log(e);
+var yaml = require('js-yaml');
+var fs = require('fs');
+var express = require('express');
+
+var app = express();
+
+function readExercises() {
+    return fs.readFileSync('./exercises.yaml', 'utf8');
 }
+
+function parseExercises() {
+    return yaml.safeLoad(readExercises());
+}
+
+app.post('/workout', function(req, res) {
+    res.set('Content-Type', 'application/json');
+    res.status(201);
+    res.send(parseExercises());
+});
+
+if (module === require.main) {
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('App listening at http://%s:%s', host, port);
+    });
+}
+
+module.exports = app;
